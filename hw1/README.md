813b2cd0b8c623e34bf168a244fd7b25d4f042bd
Anne Rogers <amr@cs.uchicago.edu>
2022-09-15 12:41:42 -0500

    Added draft materials for HW #1.  Added a basic .gitignore file

---
+++ hw1/README.md
@@ -0,0 +1,6 @@
+CMSC 14100, Autumn 2022
+Homework #1
+
+add.py: You will do your work in this file.
+
+README.md: this file
---
+++ hw1/add.py
@@ -0,0 +1,20 @@
+"""
+CMSC 14100, Autumn 2022
+Homework #1
+
+Your Name Goes Here
+
+People Consulted:
+   Complete this section
+
+Online resources consulted:
+   Complete this section   
+"""
+
+def add():
+   # Replace None with 2 + 2 in the next line.
+    return None
+
+if __name__ == "__main__":
+    print("2 + 2 is", add())
+    
