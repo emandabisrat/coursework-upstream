7b1125c50f5702cded2a627ce4370d2cddb85571
Emanda Bisrat <emandabisrat@linux2.cs.uchicago.edu>
2022-10-28 15:08:19 -0500

    done

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -16,11 +16,26 @@ Online resources consulted:
 """
 
 # A constant for the color black
+# #from pickle import TUPLE
+# from tkinter.tix import COLUMN
+
+
 BLACK = (0, 0, 0)
 
 
 def create_greyscale(image):
-    ### Replace the body of this function with your solution.
+    '''
+    A function that takes an image and returns a new greyscale version of the 
+    image. This will happen by using the equation to change every RGB value in 
+    every tuple inside the image.
+
+    Input: 
+        image (list(lists(tuples))) : only input string
+
+    Output: 
+        new_image(lst) : a list of lists of tuples
+    
+    '''
     new_image = []
     for lst1 in image:
         lst = []
@@ -30,7 +45,18 @@ def create_greyscale(image):
         new_image.append(lst)
     return new_image
 def find_region_locations(image, loc, radius):
-    ### Replace the body of this function with your solution.
+    '''
+    A function that takes an image, location (i,j), and radius and returns a new 
+    list of locations in a specified region. 
+
+    Inputs:
+        image (list) : A list of list of tuples
+        loc (i,j) : a location in terms of a list
+        radius (int) : an integer
+
+    Output:
+        region_locations (lst) : a list of tuples of locations
+    '''
     lst = []
     for i, x in enumerate(image):
         for j, item in enumerate(x):
@@ -41,25 +67,41 @@ def find_region_locations(image, loc, radius):
     return lst
     
 def blackout_region(image, loc, radius):
-    ### Replace the body of this function with your solution.
+    '''
+    A fucntion that takes an image, location, and radius and returns blacked
+    out pixels in the region of the specified radius by replacing the pixels 
+    with black pixels.
+
+    Inputs:
+        image (list) : A list of list of tuples
+        loc (i,j) : a location in terms of a list
+        radius (int) : an integer
+    
+    Output:
+        None
+    '''
     for i, x in enumerate(image):
         for j, item in enumerate(x):
             k = loc[0]
             l = loc[1]
             if (radius >= abs(i - k) and radius >= abs(j - l)):
                 image[i][j] = (BLACK)
-    
+
 
 def blur_image(image, radius):
-    ### Replace the body of this function with your solution.
+    '''
+    A function that takes 
+    '''
+    region = find_region_locations()
     new_image = []
     count = 0
-    for i in image:
+    for i in region:
+        lst = []
         for tup in i:
             red_sum = red_sum + tup[0]
             blue_sum = blue_sum + tup[1]
             green_sum = green_sum + tup[2]
             count = count + 1
-
-    new_image.append((red_sum/count, blue_sum/count, green_sum/count))
+    lst.append((red_sum/count, blue_sum/count, green_sum/count))
+    new_image.append(lst)
     return new_image
